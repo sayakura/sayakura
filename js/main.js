d07088cfe53aaca17b83591e38bffa001fb5a850
Sayakura <kura.slhs@gmail.com>
2016-05-07 18:51:21 +0000

    added some animated effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
   $('.slider').slider({indicators:false,full_width: true,height:350,transition:1000});
+  
+});
+$('#contact').mouseenter(function(){
+      $('#contact').transition({
+          scale:1.3,
+          dalay:1000,
+       });
+         $('#contact').transition({
+          scale:1,
+       });
+});
+$('.lang-logo').mouseenter(function(){
+      $(this).transition({
+          scale:1.1,
+          dalay:1000,
+       });
+         $(this).transition({
+          scale:1,
+       });
+    
 });
